d8ce9e87aadcebdea7ba4f5fa6c6a9426575f5ff
Ezequiel Muxito <kilson-key7@hotmail.com>
2025-05-07 21:28:38 -0300

    atualização index.js

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -8,6 +8,47 @@ const Login = () => {
   const [senha, setSenha] = useState("");
   const [erro, setErro] = useState("");
 
+  const handleLogin = () =>{
+    if(!email || !senha ){
+      alert("Infrome email e senha.");
+      return;
+    }
+    try{
+      // criar objeto de dados 
+      const payload = {
+        email: email,
+        senha: senha
+      }
+      // inserir dados no banco de dado do navegador 
+      localStorage.setItem('usuario', JSON.stringify());
+
+      // ler os dados do banco de dados 
+      const localStorageUsuario = localStorage.getItem('usuario');
+
+      // converter os dados para JSON
+      const usuario = JSON.stringify(localStorageUsuario)
+
+      // atualizar os ddos 
+      usuario.email = "tech@tech.com.br";
+      usuario.senha = "1234567889";
+
+      // remover objeto do banco de dados 
+      localStorage.removeItem('usuario');
+
+      // inserir novamente 
+      localStorage.setItem('usuario', JSON.stringify(payload));
+
+      if(usuario.email === email && usuario.senha === senha){
+        return;
+      }
+      else{
+        setErro("Usuário ou senha inválido");
+      }
+    }catch(erro){
+      setErro(`Erro ao fazer login. Tente novamente mais tarde. ${erro}`)
+    }
+  }
+
   return (
     <S.Container>
       <S.Content>
@@ -15,19 +56,22 @@ const Login = () => {
         <Input
           type="text"
           placeholder="Digite seu usuário"
-          value={""}
-          
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
         <Input
           type="password"
           placeholder="Digite sua senha"
-          value={""}
+          value={senha}
+          onChange={(e) => {
+            setSenha(e.target.value);
+          }}
         />
-          <Button text="Entrar" color="#0d6efd"></Button>
-          <Button
-            text="Cadastrar"
-            color="#666"
-          ></Button>
+        {erro && <S.Label>{erro}</S.Label>}
+        <Button onClick={handleLogin} text="Entrar" color="#0d6efd"></Button>
+        <Button text="Cadastrar" color="#666"></Button>
       </S.Content>
     </S.Container>
   );
